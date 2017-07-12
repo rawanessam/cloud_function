f8e7480b0e5fd339f3be2bac14ab1eeb06f9fac6
Rawan <rawanessam34@gmail.com>
2017-07-12 10:19:04 +0200

    basic post request

--- functions/index.js
+++ functions/index.js
@@ -4,5 +4,17 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hi!");
+  let config = require('./test.json');
+  // request.body
+  let toSend=request.body
+  toSend.newVal="hello again"
+  toSend.hello="No "
+  //request.assert('email','this isn\'t an email').isEmail();
+  // let errors=request.validationErrors();
+  if(typeof toSend.email !="string" )
+  {
+    // console.log(errors)
+    return response.status(400).send('bad email')
+  }
+  response.json(toSend);
  });
