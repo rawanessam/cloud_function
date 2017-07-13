c1c8c76af847155d9ae5b9ed85a591e89de76d12
Rawan <rawanessam34@gmail.com>
2017-07-13 12:42:55 +0200

    add_experience

--- functions/index.js
+++ functions/index.js
@@ -3,18 +3,27 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+const querystring = require('querystring');
+ 
+
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  let config = require('./test.json');
+ 
+let responseObject={
+"speech": "Barack Hussein Obama II was the 44th and current President of the United States.",
+"displayText": "Barack Hussein Obama II was the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+"data": {},
+"contextOut": [],
+"source": "DuckDuckGo"
+}
   // request.body
-  let toSend=request.body
-  toSend.newVal="hello again"
-  toSend.hello="No "
-  //request.assert('email','this isn\'t an email').isEmail();
-  // let errors=request.validationErrors();
-  if(typeof toSend.email !="string" )
-  {
-    // console.log(errors)
-    return response.status(400).send('bad email')
-  }
-  response.json(toSend);
+  console.log(request.body.result.parameters)
+  
+  
+  let company=request.body.result.parameters.companies
+  let position= request.body.result.parameters.Positions
+  let duration = request.body.result.parameters.duration
+  responseObject.speech=requestBody
+  
+  response.send(responseObject);
  });
